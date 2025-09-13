725232843afa6f81de37593a1d7289021513da0c
trunggcao <trungcao2004@gmail.com>
2025-09-13 22:52:47 +0700

    chapter 3

---
+++ src/chapter 3/chapter3.js
@@ -0,0 +1,18 @@
+
+
+
+// Object
+const person = {
+    name: "Cao Quoc Trung",
+    age : 21,
+    address : "Long Xuyen - An Giang"
+}
+
+console.log(person)
+
+
+// array
+
+const myScores = [ 3 ,4 , 8, 8]
+
+console.log(myScores)
