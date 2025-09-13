d0ba7e87c82971547517a4871471ac770825e9d3
trunggcao <trungcao2004@gmail.com>
2025-09-13 22:42:51 +0700

    chapter 2

---
+++ src/chapter 2/chapter2.js
@@ -0,0 +1,10 @@
+console.log("Chapter 2");
+
+
+let myName = "Trung cao";
+const myAge = "21";
+
+
+console.log("Thong tin cua trung dep trai: ")
+console.log("Ho va ten: " + myName);
+console.log(myAge);
